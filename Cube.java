75e0abe8e4ae407b53582c5eddc8e7f7867057d2
DaneIwema <daneiwema@gmail.com>
2023-10-19 10:02:54 -0500

    started rotation of vertices, still getting out of bounds errors

--- Cube.java
+++ Cube.java
@@ -1,23 +1,48 @@
 public class Cube {
+    public static int cubeSize = 3;
+    public static int height = cubeSize*4, width = cubeSize*8;
+    public static int [] center = {height/2, width/2};
     public static char [] faces = {'@', '#', '$', '%', '&', '*'};
-    public static int rotateAmount = 5;
+    public static int rotateAmount = 180;
     public static double distanceFromCam = 10;
+    public static int [][] verticies = {
+        {center[0] - cubeSize, center[1] - cubeSize*2}, 
+        {center[0] - cubeSize, center[1] + cubeSize*2}, 
+        {center[0] + cubeSize, center[1] - cubeSize*2}, 
+        {center[0] + cubeSize, center[1] + cubeSize*2}
+    };
     public static void main(String [] args){
-        while(true){
-            StringBuilder stringbuilder = new StringBuilder();
-            
-        }
+            String [][] display = new String[height][width];
+            display[center[0]][center[1]] = "*";
+            for (int i = 0; i < verticies.length; i++){
+                display[rotateY(verticies[i][1], verticies[i][0])][rotateX(verticies[i][1], verticies[i][0])] = "*";
+            }
+            System.out.print(toString(display));
     }
 
-    public int rotateX(int x, int y){
-        return (x*(int)Math.cos(Math.toRadians(rotateAmount))) - (y*(int)Math.sin(Math.toRadians(rotateAmount)));
+    public static int rotateX(int x, int y){
+        return ((center[1]-x)*(int)Math.cos(Math.toRadians(rotateAmount))) - ((center[0]-y)*(int)Math.sin(Math.toRadians(rotateAmount)));
     }
 
-    public int rotateY(int x, int y){
-        return (x*(int)Math.sin(Math.toRadians(rotateAmount))) - (y*(int)Math.cos(Math.toRadians(rotateAmount)));
+    public static int rotateY(int x, int y){
+        return ((center[1]-x)*(int)Math.sin(Math.toRadians(rotateAmount))) - ((center[0]-y)*(int)Math.cos(Math.toRadians(rotateAmount)));
     }
 
     // public int drawVector(int x, int y){
-
+        
     // }
+
+    public static String toString(String [][] display){
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < height; i++){
+            for (int j = 0; j < width; j ++){
+                if (display[i][j] != null)
+                    builder.append(display[i][j]);
+                else
+                    builder.append(" ");
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
 }
