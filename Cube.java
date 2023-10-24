3ea171590be4c3d391cfa13c8907576376b82cb8
DaneIwema <daneiwema@gmail.com>
2023-10-24 15:46:35 -0500

    drawLine() algorithm works

--- Cube.java
+++ Cube.java
@@ -6,24 +6,18 @@ public class Cube {
 
     public static int [] center = {width/2, height/2};
 
-    public static int [][] corners = new int [4][2];
+    public static double [][] corners = new double [4][2];
 
     public static char [] faces = {'@', '#', '$', '%', '&', '*'};
 
-    public static String [][] display = new String[width][height];
+    public static String [][] display;
 
     public static void main(String [] args){
+        display = new String[width][height];
         initializeCorners();
         drawCorners();
-        display[center[0]][center[1]] = "0";
-        drawLine(center[0], center[1], corners[0][0], corners[0][1]);
-        // drawLine(corners[1][0], corners[1][1], corners[2][0], corners[2][1]);
+        connectCorners();
         System.out.print(toString(display));
-        System.out.printf("Center: %d, %d%n", center[0], center[1]);
-        System.out.printf("point 1: %d, %d%n", rotateX(center[0], 45, cubeSize), rotateY(center[1], 45, cubeSize));
-        System.out.printf("point 2: %d, %d%n", rotateX(center[0], 135, cubeSize), rotateY(center[1], 135, cubeSize));
-        System.out.printf("Distance: %d%n", distance(corners[1][0], corners[1][1], corners[2][0], corners[2][1]));
-        System.out.printf("angle: %d%n", (int)getAngle(corners[1][0], corners[1][1], corners[2][0], corners[2][1]));
     }
 
     public static void initializeCorners(){
@@ -39,41 +33,40 @@ public class Cube {
         for (int i = 0; i < 4; i++){
             StringBuilder builder = new StringBuilder();
             builder.append(i);
-            display[corners[i][0]][corners[i][1]] = builder.toString();
+            display[(int)Math.round(corners[i][0])][(int)Math.round(corners[i][1])] = builder.toString();
         }
     }
 
-    public static void drawLine(int xOne, int yOne, int xTwo, int yTwo){
-        if (xOne < xTwo && yOne < yTwo){
+    public static void connectCorners(){
+        for(int i = 0; i < 4; i++){
+            drawLine(corners[i][0], corners[i][1], corners[i+1][0], corners[i+1][1]);
+        }
+    }
 
+    public static void drawLine(double xOne, double yOne, double xTwo, double yTwo){
+        double m = (yTwo - yOne)/(xTwo - xOne);
+        double b = yOne - (m*xOne);
+        if (xOne < xTwo){
+            for (int x = (int)Math.round(xOne); x <= xTwo; x++){ 
+                int y = (int)Math.round((m*x) + b); 
+                display[x][y] = "x"; 
+            } 
         }
         else{
-            
+            for (int x = (int)Math.round(xTwo); x <= xOne; x++){ 
+                int y = (int)Math.round((m*x) + b); 
+                display[x][y] = "x"; 
+            } 
         }
-        int m = (yTwo - yOne) / (xTwo - xOne); 
-        for (int x = xOne; x <= xTwo; x++){ 
-            
-            // Assuming that the round function finds 
-            // closest integer to a given float. 
-            int y = Math.round((m*x) + c); 
-            display[x][y] = "x"; 
-        } 
+        
     }
 
-    public static int rotateX(int xC, int rotateAmount, double radius){
-        return xC+ (int)Math.round((radius*2) * Math.cos(Math.toRadians(rotateAmount)));
+    public static double rotateX(int xC, int rotateAmount, double radius){
+        return xC+ Math.round((radius*2) * Math.cos(Math.toRadians(rotateAmount)));
     }
 
-    public static int rotateY(int yC, int rotateAmount, double radius){
-        return yC + (int)Math.round(radius * Math.sin(Math.toRadians(rotateAmount)));
-    }
-
-    public static int getAngle(int x, int y, int xTwo, int yTwo){
-        return (int)Math.round(Math.atan2(yTwo - y, xTwo - x) * 180 / Math.PI);
-    }
-
-    public static int distance(int xOne, int yOne, int xTwo, int yTwo){
-        return (int)Math.round(Math.sqrt(((Math.pow((xTwo - xOne), 2)) + (Math.pow((yTwo - yOne), 2)))));
+    public static double rotateY(int yC, int rotateAmount, double radius){
+        return yC + Math.round(radius * Math.sin(Math.toRadians(rotateAmount)));
     }
 
     public static String toString(String [][] display){
@@ -87,9 +80,16 @@ public class Cube {
                 else
                     builder.append(" ");
             }
-            builder.append(i);
             builder.append("\n");
         }
         return builder.toString();
+    }
+
+    public static int getAngle(int x, int y, int xTwo, int yTwo){
+        return (int)Math.round(Math.atan2(yTwo - y, xTwo - x) * 180 / Math.PI);
+    }
+
+    public static int distance(int xOne, int yOne, int xTwo, int yTwo){
+        return (int)Math.round(Math.sqrt(((Math.pow((xTwo - xOne), 2)) + (Math.pow((yTwo - yOne), 2)))));
     }
 }
