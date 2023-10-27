62e0f6c90e7410bdff62fc6a731ec72283cd110b
DaneIwema <daneiwema@gmail.com>
2023-10-27 11:27:47 -0500

    I did it!

--- Cube.java
+++ Cube.java
@@ -1,71 +1,126 @@
 public class Cube {
-    public static double cubeRad = 10;
+    public static float r = 7.0f;
 
-    public static double A, B;
+    public static float A, B, C;
 
-    public static int height = (int)(cubeRad*2.5);
-    public static int width = (int)cubeRad*5;
-    public static int length = (int)cubeRad*5;
+    public static int height = (int)(r * 5) + 1;
+    public static int width = (int)Math.round(r*12.5f) + 1;
 
-    public static int [] center = {width/2, height/2, length/2};
+    public static int x = width/2;
+    public static int y = height/2;
 
-    public static double [][] corners = new double [8][2];
-
-    public static String [] greyScale = {".", ":", "-", "=", "+", "*", "#", "%", "@"};
-
-    public static String [][] display;
+    public static char [][] buffer;
+    public static float [][] zBuffer;
+    public static char [] faces = {'@', '#', '$', '%', '&', '*'};
 
     public static void main(String [] args) throws InterruptedException{
-        double[] coord = new double[] {0, 0};
-        display = new String[width][height];
-        display[center[0]][center[1]] = "0";
-
-        for (coord[0] = 1; coord[0] < 180; coord[0] = coord[0] + 2){
-            // double slot = (rotateZ(coord));
-            // System.out.println(rti(slot / ((cubeRad*2)/greyScale.length)) + 4);
-            toDisplay(rotateX(coord),rotateY(coord), rotateZ(coord));
-            // System.out.print(toString(display));
-            Thread.sleep(50);
+        float [][] cubeVerticies = new float [][] {
+            {-r, -r, -r}, {-r, -r, r}, {-r, r, -r}, {-r, r, r},
+            {r, -r, -r}, {r, -r, r}, {r, r, -r}, {r, r, r}
+        };
+        int[][] cubeEdges = {
+            {0, 1}, {1, 3}, {3, 2}, {2, 0},
+            {4, 5}, {5, 7}, {7, 6}, {6, 4},
+            {0, 4}, {1, 5}, {2, 6}, {3, 7}
+        };
+        zBuffer = new float [width][height];
+        // buffer[x][y] = '0';
+        while (true){
+            buffer = new char [width][height];
+            A += 0.05f;
+            B += 0.05f;
+            C += 0.01f;
+            for (int i = 0; i < 12; i++) {
+                drawVector(
+                    (int)Math.round(rotateX(cubeVerticies[cubeEdges[i][0]])*2) + x,
+                    (int)Math.round(rotateY(cubeVerticies[cubeEdges[i][0]])) + y,
+                    (int)Math.round(rotateX(cubeVerticies[cubeEdges[i][1]])*2) + x,
+                    (int)Math.round(rotateY(cubeVerticies[cubeEdges[i][1]])) + y
+                );
+            }
+            System.out.print(toDisplay(buffer));
+            Thread.sleep(80);
         }
     }
 
-    public static void initializeCorners(){
-        
+    public static float rotateX(float [] v){
+        return (float)(v[1] * Math.sin(A) * Math.sin(B) * Math.cos(C) - v[2] * Math.cos(A) * Math.sin(B) * Math.cos(C) +
+         v[1] * Math.cos(A) * Math.sin(C) + v[2] * Math.sin(A) * Math.sin(C) + v[0] * Math.cos(B) * Math.cos(C));
     }
 
-    public static void drawLine(double [] coordOne, double [] coordTwo){
-        
+    public static float rotateY(float [] v) {
+        return (float)(v[1] * Math.cos(A) * Math.cos(C) + v[2] * Math.sin(A) * Math.cos(C) -
+               v[1] * Math.sin(A) * Math.sin(B) * Math.sin(C) + v[2] * Math.cos(A) * Math.sin(B) * Math.sin(C) -
+               v[0] * Math.cos(B) * Math.sin(C));
     }
 
-    public static double rotateX(double [] coord){
-        return cubeRad * Math.sin(Math.toRadians(coord[0])) * Math.cos(Math.toRadians(coord[1]));
+    public static float rotateZ(float [] v) {//int i, int j, int k
+        return (float)(v[2] * Math.cos(A) * Math.cos(B) - v[1] * Math.sin(A) * Math.cos(B) + v[0] * Math.sin(B));
     }
 
-    public static double rotateY(double [] coord){
-        return cubeRad * Math.sin(Math.toRadians(coord[0])) * Math.sin(Math.toRadians(coord[1]));
+    public static void drawVector(int x0, int y0, int x1, int y1){
+        if (Math.abs(y1 - y0) < Math.abs(x1 - x0))
+            if (x0 > x1)
+                plotLineLow(x1, y1, x0, y0);
+            else
+                plotLineLow(x0, y0, x1, y1);
+        else
+            if (y0 > y1)
+                plotLineHigh(x1, y1, x0, y0);
+            else
+                plotLineHigh(x0, y0, x1, y1);
     }
 
-    public static double rotateZ(double [] coord){
-        return cubeRad * Math.cos(Math.toRadians(coord[0]));
+    public static void plotLineLow(int x0, int y0, int x1, int y1){
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int yi = 1;
+        if (dy < 0){
+            yi = -1;
+            dy = -dy;
+        }
+        int D = (2 * dy) - dx;
+        int y = y0;
+        for (int x = x0; x < x1; x++){
+            buffer[x][y] = 'x';
+            if (D > 0) {
+                y = y + yi;
+                D = D + (2 * (dy - dx));
+            }
+            else
+                D = D + 2*dy;
+        }
     }
 
-    private static void toDisplay(double x, double y, double z){
+    public static void plotLineHigh(int x0, int y0, int x1, int y1){
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int xi = 1;
+        if (dx < 0) {
+            xi = -1;
+            dx = -dx;
+        }
+        int D = (2 * dx) - dy;
+        int x = x0;
 
-
-
-
-        int depth = rti(z / ((cubeRad*2)/greyScale.length)) + 4;
-        System.out.println(depth+4);
-        // display[(center[0] + (rti((x))*2))][center[1] + rti(y)] = greyScale[depth+4];
+        for (int y = y0; y < y1; y++){
+            buffer[x][y] = 'x';
+            if (D > 0) {
+                x = x + xi;
+                D = D + (2 * (dx - dy));
+            }  
+            else
+                D = D + 2*dx;
+        }
     }
-    
-    public static String toString(String [][] display){
+
+    public static String  toDisplay(char [][] display){
         StringBuilder builder = new StringBuilder();
         builder.append("\033[2J");
         builder.append("\033[H");
         for(int i = height-1; i > -1; i--){
             for (int j = 0; j < width; j++){
-                if (display[j][i] != null)
+                if (display[j][i] != '\u0000')
                     builder.append(display[j][i]);
                 else
                     builder.append(" ");
@@ -73,9 +128,5 @@ public class Cube {
             builder.append("\n");
         }
         return builder.toString();
-    }
-
-    public static int rti(double num){
-        return (int)Math.round(num);
     }
 }
