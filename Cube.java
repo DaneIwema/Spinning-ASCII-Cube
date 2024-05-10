c4c73e7f18eb16558fa4cb827dd1be0afb926d33
DaneIwema <daneiwema@gmail.com>
2024-05-10 12:25:52 -0500

    Completed Draw FaceMethod
    draws faces to the cube and each face is a customizable char. Only bug is that faces are drawn in same order every time and when the front face is behind the back face it will still draw to the front, I will add normals and sorting them in the future.

--- Cube.java
+++ Cube.java
@@ -1,18 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Cube {
-    public static float r = 7.0f;
+    public static int r = 7;
 
     public static float A, B, C;
 
-    public static int height = (int)(r * 5) + 1;
+    public static int height = r * 5 + 1;
     public static int width = (int)Math.round(r * 12.5f) + 1;
 
-    public static int x = width/2;
-    public static int y = height/2;
+    public static int posX = width/2;
+    public static int posY = height/2;
 
-    public static char [] buffer;
+    public static char [][] buffer;
     public static char [] faces = {'@', '#', '$', '%', '&', '*'};
-    public static int [][] line1;
-    public static int [][] line2;
+    public static Queue<int[]> line1;
+    public static Queue<int[]> line2;
 
     public static long now;
     public static long updateTime;
@@ -20,33 +23,36 @@ public class Cube {
 
     public static int targetFPS = 16;
 
+    public static int [][] cubeVerticies = new int [][] {
+        {-r, r, -r},         {r, r, -r},   // 0, 1
+
+            {-r, r, r},        {r, r, r},  // 2, 3
+
+        {-r, -r, -r},       {r, -r, -r},   // 4, 5
+
+            {-r, -r, r},        {r, -r, r} // 6, 7
+    };
+    public static int[][] cubeFaces = {
+        {0, 1, 2, 3}, {4 ,5 ,6 ,7}, {4, 6, 0, 2}, {1, 3, 5, 7}, {2, 3, 6, 7}, {0, 1, 4, 5}
+    };
+    public static float[][] faceNormals = {
+
+    };
+
     public static void main(String [] args) throws InterruptedException{
-        float [][] cubeVerticies = new float [][] {
-            {-r, -r, -r}, {-r, -r, r}, {-r, r, -r}, {-r, r, r},
-            {r, -r, -r}, {r, -r, r}, {r, r, -r}, {r, r, r}
-        };
-        int[][] cubeEdges = {
-            {0, 1}, {1, 3}, {3, 2}, {2, 0},
-            {4, 5}, {5, 7}, {7, 6}, {6, 4},
-            {0, 4}, {1, 5}, {2, 6}, {3, 7}
-        };
+        
         
         wait = 34;
         while (true){
 
             now = System.currentTimeMillis();
             if (wait > targetFPS) {
-                buffer = new char [width * height];
+                buffer = new char [height][width];
                 A += 0.05f;
                 B += 0.05f;
                 C += 0.01f;
-                for (int i = 0; i < 12; i++) {
-                    drawVector(
-                        (int)Math.round(rotateX(cubeVerticies[cubeEdges[i][0]])*2) + x,
-                        (int)Math.round(rotateY(cubeVerticies[cubeEdges[i][0]])) + y,
-                        (int)Math.round(rotateX(cubeVerticies[cubeEdges[i][1]])*2) + x,
-                        (int)Math.round(rotateY(cubeVerticies[cubeEdges[i][1]])) + y
-                    );
+                for (int i = 0; i < 6; i++) {
+                    drawFace( cubeFaces[i], faces[i]);
                 }
                 System.out.print(toDisplay(buffer));
                 wait = 0;
@@ -57,140 +63,83 @@ public class Cube {
         }
     }
 
-    public float [] rotate(float [] coord){
-        float [] newCoord = {rotateX(coord), rotateY(coord), rotateZ(coord)};
-        return newCoord;
-    }
-
-    public static float rotateX(float [] v){
-        return (float)(v[1] * Math.sin(A) * Math.sin(B) * Math.cos(C) - v[2] * Math.cos(A) * Math.sin(B) * Math.cos(C) +
+    public static int rotateX(int [] v){
+        return (int)Math.round(v[1] * Math.sin(A) * Math.sin(B) * Math.cos(C) - v[2] * Math.cos(A) * Math.sin(B) * Math.cos(C) +
          v[1] * Math.cos(A) * Math.sin(C) + v[2] * Math.sin(A) * Math.sin(C) + v[0] * Math.cos(B) * Math.cos(C));
     }
 
-    public static float rotateY(float [] v) {
-        return (float)(v[1] * Math.cos(A) * Math.cos(C) + v[2] * Math.sin(A) * Math.cos(C) -
+    public static int rotateY(int [] v) {
+        return (int)Math.round(v[1] * Math.cos(A) * Math.cos(C) + v[2] * Math.sin(A) * Math.cos(C) -
                v[1] * Math.sin(A) * Math.sin(B) * Math.sin(C) + v[2] * Math.cos(A) * Math.sin(B) * Math.sin(C) -
                v[0] * Math.cos(B) * Math.sin(C));
     }
 
-    public static float rotateZ(float [] v) {
-        return (float)(v[2] * Math.cos(A) * Math.cos(B) - v[1] * Math.sin(A) * Math.cos(B) + v[0] * Math.sin(B));
-    }
-
-    /* TODO drawFace(int[] face) method */
-    public static void drawFace(int[] face) {
-
-        line1 = new int[][2];
+    /* TODO drawFace method */
+    public static void drawFace(int[] face, char color) {
 
         /* Draws from vec0 to vec1 and vec2 to vec3 */
-        int x0 = face[0], y0 = face[1]; //vec0
-        int x1 = face[2], y1 = face[3]; //vec1
-        int x2 = face[4], y2 = face[5]; //vec2
-        int x3 = face[6], y3 = face[7]; //vec3
+        int[] vec0 = {rotateX(cubeVerticies[face[0]]) * 2 + posX, rotateY(cubeVerticies[face[0]]) + posY}; //vec0
+        int[] vec1 = {rotateX(cubeVerticies[face[1]]) * 2 + posX, rotateY(cubeVerticies[face[1]]) + posY}; //vec1
+        int[] vec2 = {rotateX(cubeVerticies[face[2]]) * 2 + posX, rotateY(cubeVerticies[face[2]]) + posY}; //vec2
+        int[] vec3 = {rotateX(cubeVerticies[face[3]]) * 2 + posX, rotateY(cubeVerticies[face[3]]) + posY}; //vec3
 
-        /* Setup from vec0 to vec1 */
+        line1 = new LinkedList<>();
+        line2 = new LinkedList<>();
+
+        drawVector(vec0, vec1, color, line1);
+        drawVector(vec2, vec3, color, line2);
+
+        while (!line1.isEmpty() && !line2.isEmpty()) {
+            int[] pair1 = line1.poll();
+            int[] pair2 = line2.poll();
+            drawVector(pair1, pair2, color, null);
+        }
+    }
+
+    public static void drawVector (int[] pair1, int[] pair2, char color, Queue<int[]> q) {
+        int x0 = pair1[0], y0 = pair1[1];
+        int x1 = pair2[0], y1 = pair2[1];
+
         int dx = Math.abs(x1 - x0);
         int sx = x0 < x1 ? 1 : -1;
         int dy = -Math.abs(y1 - y0);
         int sy = y0 < y1 ? 1 : -1;
         int error = dx + dy;
-
-        /* Setup for vec2 to vec3 */
-        int dx2 = Math.abs(x3 - x2);
-        int sx2 = x2 < x3 ? 1 : -1;
-        int dy2 = -Math.abs(y3 - y2);
-        int sy2 = y2 < y3 ? 1 : -1;
-        int error2 = dx2 + dy2;
         
-        while (true) {
-
-            buffer[y0 * width + x0] = '*';
-
-
+        while (true){
+            if (q == null) {
+                buffer[y0][x0] = color;
+            } else {
+                int[] pair = {x0, y0};
+                q.add(pair);
+            }
             if (x0 == x1 && y0 == y1) break;
             int e2 = 2 * error;
-            if (e2 >= dy) {
+            if (e2 >= dy){
                 if (x0 == x1) break;
                 error = error + dy;
                 x0 = x0 + sx;
             }
-            if (e2 <= dx) {
+            if (e2 <= dx){
                 if (y0 == y1) break;
                 error = error + dx;
                 y0 = y0 + sy;
             }
         }
-
-
-
     }
 
-    public static void drawVector(int x0, int y0, int x1, int y1){
-        if (Math.abs(y1 - y0) < Math.abs(x1 - x0))
-            if (x0 > x1)
-                plotLineLow(x1, y1, x0, y0);
-            else
-                plotLineLow(x0, y0, x1, y1);
-        else
-            if (y0 > y1)
-                plotLineHigh(x1, y1, x0, y0);
-            else
-                plotLineHigh(x0, y0, x1, y1);
-    }
-
-    public static void plotLineLow(int x0, int y0, int x1, int y1){
-        int dx = x1 - x0;
-        int dy = y1 - y0;
-        int yi = 1;
-        if (dy < 0){
-            yi = -1;
-            dy = -dy;
-        }
-        int D = (2 * dy) - dx;
-        int y = y0;
-        for (int x = x0; x < x1; x++){
-            buffer[y*width+x] = '*';
-            if (D > 0) {
-                y = y + yi;
-                D = D + (2 * (dy - dx));
-            }
-            else
-                D = D + 2*dy;
-        }
-    }
-
-    public static void plotLineHigh(int x0, int y0, int x1, int y1){
-        int dx = x1 - x0;
-        int dy = y1 - y0;
-        int xi = 1;
-        if (dx < 0) {
-            xi = -1;
-            dx = -dx;
-        }
-        int D = (2 * dx) - dy;
-        int x = x0;
-        for (int y = y0; y < y1; y++){
-            buffer[y*width+x] = '*';
-            if (D > 0) {
-                x = x + xi;
-                D = D + (2 * (dx - dy));
-            }  
-            else
-                D = D + 2*dx;
-        }
-    }
-
-    public static String  toDisplay(char [] display){
+    public static String  toDisplay(char [][] display) {
         StringBuilder builder = new StringBuilder();
         builder.append("\033[2J");
         builder.append("\033[H");
-        for(int i = 0; i <height*width; i++){
-            if(buffer[i]=='\0')
-                builder.append(" ");
-            else
-                builder.append(buffer[i]);
-            if ((i + 1)% width == 0)
-                builder.append("\n");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+               if (buffer[i][j] == '\0')
+                    builder.append(" ");
+                else
+                    builder.append(buffer[i][j]);
+            }
+            builder.append("\n");
         }
         return builder.toString();
     }
